c7752bc9e165ade6040e23541f0af549ab3b61de
Saddan Hussain <hussainsaddan786@gmail.com>
2022-09-06 10:39:26 +0530

    changes in Q9

--- DataStruct/src/com/hussaincode/javaIntro/arrays05/Q9.java
+++ DataStruct/src/com/hussaincode/javaIntro/arrays05/Q9.java
@@ -12,13 +12,13 @@ public class Q9 {
     static int[] createTargetArray(int[] nums, int[] index) {
 
         ArrayList<Integer> al = new ArrayList<>();
-        int[] result = new int[nums.length];
+        int[] target = new int[nums.length];
         for (int i = 0; i < nums.length; i++){
             al.add(index[i], nums[i]);
         }
         for( int i = 0; i < nums.length; i++){
-            result[i] = al.get(i);
+            target[i] = al.get(i);
         }
-        return result;
+        return target;
     }
 }
