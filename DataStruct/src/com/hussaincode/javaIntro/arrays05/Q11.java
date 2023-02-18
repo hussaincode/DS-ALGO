bd093a129a4be3733ce52ab3f4d3acc38dcf4086
abuintakhab <hussainsaddan786@gmail.com>
2023-02-18 23:16:25 +0530

    Binary Search

--- DataStruct/src/com/hussaincode/javaIntro/arrays05/Q11.java
+++ DataStruct/src/com/hussaincode/javaIntro/arrays05/Q11.java
@@ -1,7 +1,36 @@
 package com.hussaincode.javaIntro.arrays05;
 
+import java.util.List;
+
 public class Q11 {
     public static void main(String[] args) {
-        
+
+        //https://leetcode.com/problems/count-items-matching-a-rule/description/
+    }
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count=0;
+
+        switch(ruleKey){
+
+            case "type"  :   for(int i=0; i<items.size(); i++ ){
+                if(items.get(i).get(0).equals(ruleValue))
+                    count++;
+            }
+                return count;
+
+            case "color" :for(int i=0; i<items.size(); i++ ){
+                if(items.get(i).get(1).equals(ruleValue) )
+                    count++;
+            }
+                return count;
+
+            case "name"  :    for(int i=0; i<items.size(); i++ ){
+                if(items.get(i).get(2).equals(ruleValue))
+                    count++;
+            }
+                return count;
+        }
+
+        return count;
     }
 }
