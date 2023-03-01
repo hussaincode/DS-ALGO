3364519291dac75a507b270dd9413777eb9b176a
abuintakhab <hussainsaddan786@gmail.com>
2023-03-02 00:12:08 +0530

    Missing Number

---
+++ DataStruct/src/com/hussaincode/javaIntro/sorting/cyclicsort/MissingNumber.java
@@ -0,0 +1,34 @@
+package com.hussaincode.javaIntro.sorting.cyclicsort;
+
+//https://leetcode.com/problems/missing-number/description/
+//Amazon question
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] nums = {3,0,1};
+        System.out.println(missingNumber(nums));
+    }
+    static int missingNumber(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i];
+            if (nums[i]<nums.length && nums[i]!=nums[correct]) {
+                swap(nums,i,correct);
+            }else {
+                i++;
+            }
+        }
+        //search for first missing number
+        for(int index=0;index<nums.length;index++){
+            if(nums[index]!=index){
+                return index;
+            }
+        }
+        //case 2
+        return nums.length;
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
