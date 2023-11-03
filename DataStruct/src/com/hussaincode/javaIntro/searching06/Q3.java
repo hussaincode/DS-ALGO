fabbeac4aa3023ee7756214998102ab05a66b67c
MS Hussain <hussainsaddan786@gmail.com>
2023-11-03 22:37:41 +0530

    binary search

---
+++ DataStruct/src/com/hussaincode/javaIntro/searching06/Q3.java
@@ -0,0 +1,22 @@
+package com.hussaincode.javaIntro.searching06;
+
+//https://leetcode.com/problems/first-bad-version/
+public class Q3 {
+    public static void main(String[] args) {
+
+    }
+    public int firstBadVersion(int n) {
+        int start =1, end =n;
+        int ans = -1;
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            if(isBadVersion(mid)==true){
+                ans = mid;
+                end = mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+}
