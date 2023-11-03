73222671d08d0651e891458f1bfde1b2beb068d9
MS Hussain <hussainsaddan786@gmail.com>
2023-11-03 19:02:43 +0530

    binary search

---
+++ DataStruct/src/com/hussaincode/javaIntro/searching06/Q1.java
@@ -0,0 +1,27 @@
+package com.hussaincode.javaIntro.searching06;
+
+//https://leetcode.com/problems/sqrtx/description/
+public class Q1 {
+    public static void main(String[] args) {
+        int x =4;
+        System.out.println(mySqrt(x));
+    }
+    public static int mySqrt(int x) {
+        long start =1, end =x;
+        long ans = 1;
+        if(x==0 || x==1){
+            return x;
+        }
+        while(start<=end){
+            long mid = start+(end-start)/2;
+            if(mid*mid<=x){
+                ans = mid;
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return (int)ans;
+    }
+
+}
