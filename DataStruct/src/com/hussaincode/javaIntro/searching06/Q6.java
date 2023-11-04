c70e1c93d7ff2e01e776a9605d9af86460a65170
MS Hussain <hussainsaddan786@gmail.com>
2023-11-04 12:44:02 +0530

    binary search

---
+++ DataStruct/src/com/hussaincode/javaIntro/searching06/Q6.java
@@ -0,0 +1,23 @@
+package com.hussaincode.javaIntro.searching06;
+
+public class Q6 {
+    public static void main(String[] args) {
+        int n =5;
+        System.out.println(arrangeCoins(n));
+    }
+    public static int arrangeCoins(int n) {
+        long start =1,end=n;
+        long res=0;
+        while(start<=end){
+            long mid = start+(end-start)/2;
+            long coins = (mid)*(mid+1)/2;
+            if(coins>n){
+                end = mid-1;
+            }else{
+                start=mid+1;
+                res = Math.max(mid,res);
+            }
+        }
+        return (int)res;
+    }
+}
