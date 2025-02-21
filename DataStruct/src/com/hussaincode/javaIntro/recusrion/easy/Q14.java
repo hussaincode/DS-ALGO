13eae29ad15e88b1f2ed86538386ce8ec767aef8
MS Hussain <hussainsaddan786@gmail.com>
2025-02-22 02:04:08 +0530

    Recursion product of 2 number

--- DataStruct/src/com/hussaincode/javaIntro/recusrion/easy/Q14.java
+++ DataStruct/src/com/hussaincode/javaIntro/recusrion/easy/Q14.java
@@ -1,4 +1,15 @@
 package com.hussaincode.javaIntro.recusrion.easy;
 
 public class Q14 {
+    public static void main(String[] args) {
+        int a = 5, b = 4;
+
+        int c = prod(a,b);
+        System.out.println(c);
+    }
+
+    private static int prod(int a, int b) {
+        if(b==1) return a;
+        return  a+prod(a,b-1);
+    }
 }
